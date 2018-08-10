11fce6602917660e0223bd9b52e60265b288a0a1
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2018-08-10 17:48:14 +0800

    needs some more tinkering

--- split_layers.py
+++ split_layers.py
@@ -3,6 +3,15 @@
 import gdspy
 import os
 
+layer_mapping = {
+	'pwell' : {
+		41: 1,
+	},
+	'nwell' : {
+		42: 1,
+	}
+}
+
 cellname="L500_MOSFET_aligning"
 
 magic_script="\n"
@@ -28,27 +37,28 @@ magic_script+="gds label no"
 magic_script+="\n"
 magic_script+="gds merge yes"
 magic_script+="\n"
-magic_script+="gds write "+cellname
+magic_script+="gds write gds/"+cellname
 magic_script+="\n"
-magic_script+="calma write "+cellname
+magic_script+="calma write gds/"+cellname
 magic_script+="\n"
 magic_script+="quit -noprompt"
 magic_script+="\n"
 
+print(os.popen("mkdir -p gds").read())
 print(os.popen("magic -dnull -noconsole << EOF"+magic_script+"EOF").read())
 
-gdsii=gdspy.GdsLibrary()
-gdsii.read_gds(
-	cellname+".gds",
-	layers={
-		41: 1,
-		42: 1,
-	}
-)
-cell=gdsii.extract(cellname)
-cell=cell.flatten()
-cell.remove_polygons(lambda pts, layer, datatype: layer != 1)
-newgdsii=gdspy.GdsLibrary(name="top")
-newgdsii.add(cell)
-newgdsii.write_gds("test.gds")
-#gdspy.LayoutViewer()
+
+for layername in layer_mapping:
+	gdsii=gdspy.GdsLibrary(layername)
+	gdsii.read_gds(
+		"gds/"+cellname+".gds",
+		layers=layer_mapping[layername],
+		rename={cellname:layername},
+	)
+	#cell=gdsii.extract(layername)
+	#cell.name=layername
+	#cell=cell.flatten()
+	#cell.remove_polygons(lambda pts, layer, datatype: layer != 1)
+	#newgdsii=gdspy.GdsLibrary("mask_"+layername)
+	#newgdsii.add(cell)
+	#newgdsii.write_gds("gds/mask_"+layername+".gds")
